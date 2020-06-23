5ccf751156dc5aee21bbef34ef73f5cac63d2384
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-23 22:27:44 +0200

    progress

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -8,28 +8,34 @@
     </div>
     --}}
 
-    <div class="row row-cols-1 row-cols-md-2 mt-4">
+    <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 mt-4">
         @foreach ($users as $user)
             <div class="col mb-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $user->getFullName()}}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">
-                            User since: {{ $user->created_at }}<br>
-                        </h6>
+                <a
+                    href="{{ Route('users.show', $user->id) }}"
+                    class="a-reset"
+                >
+                    <div class="card hover-translate-up">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $user->getFullName()}}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">
+                                User since: {{ $user->created_at }}
+                            </h6>
+                        </div>
+                        <img src="{{ $user->info->avatar }}" class="card-img-top" alt="Not available">
+                        <section class="card-body">
+                            <h6 class="card-subtitle mb-2 text-muted">
+                                Latest activity:
+                            </h6>
+                            @dump($user->getLatestActivity(5))
+                            @foreach ($user->getLatestActivity(5) as $item)
+                                @dump($item->getTable())
+                                @dump(substr($item->body, 0, 100))
+                            @endforeach
+                        </section>
                     </div>
-                    <img src="{{ $user->info->avatar }}" class="card-img-top" alt="Not available">
-                    <section class="card-body">
-                        <h6 class="card-subtitle mb-2 text-muted">
-                            Latest activity:
-                        </h6>
-                        @dump($user->getLatestActivity(5))
-                        @foreach ($user->getLatestActivity(5) as $item)
-                            @dump($item->getTable())
-                            @dump(substr($item->body, 0, 100))
-                        @endforeach
-                    </section>
-                </div>
+                </a>
+
             </div>
         @endforeach
     </div>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,27 +1,30 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <div class="container">
-        <ul>
-            <li>Surname: {{ $user->id }}</li>
-            <li>Surname: {{ $user->surname }}</li>
-            <li>Surname: {{ $user->name }}</li>
-            <li>Surname: {{ $user->email }}</li>
-            <li>Surname: {{ $user->info->phone}}</li>
-            <li>Surname: {{ $user->info->address}}</li>
-            <li>Surname: <img src="{{ $user->info->avatar }}" class="user-pic" alt="pic"></li>
-            <li>
-                <ol>
-                    @foreach ($user->posts as $post)
-                        <li>
-                            <p><a href="{{ Route('posts.show', $post->id) }}">{{ $post->title }}</a></p>
-                            <p>Created at: {{ $post->created_at }}</p>
-                            <p>Updated at: {{ $post->updated_at }}</p>
-                            <p>{{ substr($post->body, 0, 150) }}</p>
-                        </li>
-                    @endforeach
-                </ol>
-            </li>
-        </ul>
+    <div class="users-index-page container">
+        <h1 class="mb-3 text-center">{{ $user->getFullName() }}</h1>
+        <div class="d-flex">
+            <ul class="list-group">
+                <li class="list-group-item">Id #: {{ $user->id }}</li>
+                <li class="list-group-item">Surname: {{ $user->surname }}</li>
+                <li class="list-group-item">Name: {{ $user->name }}</li>
+                <li class="list-group-item">Email: {{ $user->email }}</li>
+                <li class="list-group-item">Phone number: {{ $user->info->phone}}</li>
+                <li>
+                    <ol>
+                        @foreach ($user->posts as $post)
+                            <li>
+                                <p><a href="{{ Route('posts.show', $post->id) }}">{{ $post->title }}</a></p>
+                                <p>Created at: {{ $post->created_at }}</p>
+                                <p>Updated at: {{ $post->updated_at }}</p>
+                                <p>{{ substr($post->body, 0, 150) }}</p>
+                            </li>
+                        @endforeach
+                    </ol>
+                </li>
+            </ul>
+            <img src="{{ $user->info->avatar }}" alt="Not available" class="avatar">
+        </div>
+
     </div>
 @endsection
