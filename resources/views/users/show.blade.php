66f99cb262e5c6db16a035fd4c45fba06f59dd4d
Duc Thang Vu <vuducthang91@gmail.com>
2020-06-23 23:10:48 +0200

    more style

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,30 +1,57 @@
 @extends('layouts.main')
 
 @section('main-content')
-    <div class="users-index-page container">
-        <h1 class="mb-3 text-center">{{ $user->getFullName() }}</h1>
-        <div class="d-flex">
-            <ul class="list-group">
-                <li class="list-group-item">Id #: {{ $user->id }}</li>
-                <li class="list-group-item">Surname: {{ $user->surname }}</li>
-                <li class="list-group-item">Name: {{ $user->name }}</li>
-                <li class="list-group-item">Email: {{ $user->email }}</li>
-                <li class="list-group-item">Phone number: {{ $user->info->phone}}</li>
-                <li>
-                    <ol>
-                        @foreach ($user->posts as $post)
-                            <li>
-                                <p><a href="{{ Route('posts.show', $post->id) }}">{{ $post->title }}</a></p>
-                                <p>Created at: {{ $post->created_at }}</p>
-                                <p>Updated at: {{ $post->updated_at }}</p>
-                                <p>{{ substr($post->body, 0, 150) }}</p>
-                            </li>
-                        @endforeach
-                    </ol>
-                </li>
-            </ul>
-            <img src="{{ $user->info->avatar }}" alt="Not available" class="avatar">
-        </div>
+    <div class="users-show-page container">
+        <header>
+            <h1 class="mb-3 text-center">{{ $user->getFullName() }}</h1>
+            <div class="d-flex mb-3">
+                <img src="{{ $user->info->avatar }}" alt="Not available" class="avatar">
+                <ul class="list-group">
+                    <li class="list-group-item">Id #: {{ $user->id }}</li>
+                    <li class="list-group-item">Surname: {{ $user->surname }}</li>
+                    <li class="list-group-item">Name: {{ $user->name }}</li>
+                    <li class="list-group-item">Email: {{ $user->email }}</li>
+                    <li class="list-group-item">Phone number: {{ $user->info->phone}}</li>
+                </ul>
+            </div>
+        </header>
 
-    </div>
+        <section>
+            <h3 class="text-center mb-3">Activities</h3>
+            <div class="row">
+                <section class="posts col-6">
+                    <h5 class="pl-5">Posts by {{ $user->name }}</h5>
+                    @foreach ($user->posts as $post)
+                        <a href="{{ Route('posts.show', $post->id) }}" class="a-reset">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $post->title }}</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">
+                                        Created at: {{ $post->created_at }}<br>
+                                        Last update: {{ $post->updated_at }}
+                                    </h6>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+                </section>
+
+                <section class="comments">
+                    <h5 class="pl-5">Comments by {{ $user->name }}</h5>
+                    @foreach ($user->comments as $comment)
+                        <a href="{{ Route('posts.show', $post->id) }}" class="a-reset"> {{-- come fare? --}}
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">In {{ $comment->post->title }}</h5>
+                                    <h6 class="card-subtitle mb-2 text-muted">
+                                        Created at: {{ $post->created_at }}<br>
+                                        Last update: {{ $post->updated_at }}
+                                    </h6>
+                                </div>
+                            </div>
+                        </a>
+                    @endforeach
+                </section>
+            </div>
+        </section>
 @endsection
